c7ad30b0f74df9aa23d02c0928fd534ce42de757
Franjo7 <franjolovric23@gmail.com>
2023-04-16 22:07:29 +0200

    Updated DELETE function

--- views/customer/delete.php
+++ views/customer/delete.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Delete Customer</title>
-  </head>
-  <body>
-    <h2>Delete Customer</h2>
-    <form action="?controller=customers&action=deletecustomer&id=<?php echo $customer->CustomerID; ?>" method="post">
-      <p>Are you sure you want to delete this customer?</p>
-      <p><strong>Customer ID:</strong> <?php echo $customer->CustomerID; ?></p>
-      <p><strong>Company Name:</strong> <?php echo $customer->CompanyName; ?></p>
-      <p><strong>Contact Name:</strong> <?php echo $customer->ContactName; ?></p>
-      <p><strong>Contact Title:</strong> <?php echo $customer->ContactTitle; ?></p>
-      <p><strong>Address:</strong> <?php echo $customer->Address; ?></p>
-      <p><strong>City:</strong> <?php echo $customer->City; ?></p>
-      <p><strong>Region:</strong> <?php echo $customer->Region; ?></p>
-      <p><strong>Postal Code:</strong> <?php echo $customer->PostalCode; ?></p>
-      <p><strong>Country:</strong> <?php echo $customer->Country; ?></p>
-      <p><strong>Phone:</strong> <?php echo $customer->Phone; ?></p>
-      <p><strong>Fax:</strong> <?php echo $customer->Fax; ?></p>
-      <input type="hidden" name="confirmed" value="true">
-      <input type="submit" value="Delete">
-    </form>
-    <p><a href="?controller=customers">Back to Customer List</a></p>
-  </body>
-</html>
+<p>Izbrisali ste podatak iz tablice Customers. </p>
+
+<p><?php echo $customer; ?></p>
--- views/order/delete.php
+++ views/order/delete.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Delete Order</title>
-  </head>
-  <body>
-    <h2>Delete Order</h2>
-    <form action="?controller=orders&action=delete&id=<?php echo $order->OrderID; ?>" method="post">
-      <p>Are you sure you want to delete this order?</p>
-      <p><strong>Order ID:</strong> <?php echo $order->OrderID; ?></p>
-      <p><strong>Customer ID:</strong> <?php echo $order->CustomerID; ?></p>
-      <p><strong>Employee ID:</strong> <?php echo $order->EmployeeID; ?></p>
-      <p><strong>Order Date:</strong> <?php echo $order->OrderDate; ?></p>
-      <p><strong>Required Date:</strong> <?php echo $order->RequiredDate; ?></p>
-      <p><strong>Shipped Date:</strong> <?php echo $order->ShippedDate; ?></p>
-      <p><strong>Ship Via:</strong> <?php echo $order->ShipVia; ?></p>
-      <p><strong>Freight:</strong> <?php echo $order->Freight; ?></p>
-      <p><strong>Ship Name:</strong> <?php echo $order->ShipName; ?></p>
-      <p><strong>Ship Address:</strong> <?php echo $order->ShipAddress; ?></p>
-      <p><strong>Ship City:</strong> <?php echo $order->ShipCity; ?></p>
-      <p><strong>Ship Region:</strong> <?php echo $order->ShipRegion; ?></p>
-      <p><strong>Ship Postal Code:</strong> <?php echo $order->ShipPostalCode; ?></p>
-      <p><strong>Ship Country:</strong> <?php echo $order->ShipCountry; ?></p>
-      <input type="hidden" name="confirmed" value="true">
-      <input type="submit" value="Delete">
-    </form>
-    <p><a href="?controller=orders">Back to Order List</a></p>
-  </body>
-</html>
+<p>Izbrisali ste podatak iz tablice Orders. </p>
+
+<p><?php echo $order; ?></p>
